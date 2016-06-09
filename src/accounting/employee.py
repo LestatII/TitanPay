c748aec9b7fd25fb1a6b0d35b5795315a7dcd65a
Lestat II <rback1@live.spcollege.edu>
2016-06-08 23:29:07 -0400

    Committing changes for Assignment 3.

--- src/accounting/employee.py
+++ src/accounting/employee.py
@@ -1,23 +1,57 @@
-# Creation of the Employee class to store and manipulate Employee related data.
+# Creation of the Employee class to store and manipulate general employee data.
 class Employee:
 
-    # Initilization of the Employee class and its attributes.
-    def __init__(self, employee_id, first_name, last_name, hourly_rate, weekly_dues):
-        self.__employee_id = get_employee_id()  # Get employee ID.
-        self.__first_name = get_first_name()    # Get first name.
-        self.__last_name = get_last_name()    # Get last name.
-        self.__hourly_rate = get_hourly_rate()    # Get hourly rate.
-        self.__weekly_dues = get_weekly_dues()    # Get weekly dues.
+    # Initialization of the Employee class and its attributes.
+    def __init__(self, employee_id, first_name, last_name, weekly_dues):
+        self.__employee_id = employee_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__weekly_dues = weekly_dues
 
-    # get_employee_id will gather employee ID from user input.
-    def get_employee_id = input('Please input employee ID: ')
+        # The following methods are mutators for the class's data attributes.
+        def get_employee_id =
 
-    # get_first_name will gather employee first name from user input.
-    def get_first_name = input('Please enter employee first name: ')
+        def set_first_name(self, first_name):
+            self.__first_name = first_name
 
-    def get_last_name = input('Please enter employee last name: ')
+        def set_last_name(self, last_name):
+            self.__last_name = last_name
+
+        # The get_full_name function will get first and last name and order them as last_name, first_name.
+        def get_full_name(last_name, first_name):
+            return __full_name = ()
+
+    def get_employee_id(self):
+        return self.__employee_id
+
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
 
     # The get_full_name function will get first and last name and order them as last_name, first_name.
-
     def get_full_name(last_name, first_name):
         return __full_name = ()
+
+# The Hourly class holds data for an hourly employee.
+class Hourly(Employee):
+    # The __init__ method accepts arguments for the hourly employee's employee ID, first and last name, \
+    # weekly dues and hourly rate.
+    def __init__(self, employee_id, first_name, last_name, weekly_dues, hourly_rate):
+        # Call the superclass's __init__ method and pass the required arguments.
+        Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
+
+        # Initialize the hourly_rate attribute.
+        self.__hourly_rate = hourly_rate
+
+# The Salary class holds data for an salaried employee.
+class Salary(Employee):
+    # The __init__ method accepts arguments for the salaried employee's employee ID, first and last name, \
+    # weekly dues and salary.
+    def __init__(self, employee_id, first_name, last_name, weekly_dues, salary):
+        # Call the superclass's __init__ method and pass the required arguments.
+        Employee.__init__(self, employee_id, first_name, last_name, weekly_dues)
+
+        # Initialize the salary attribute.
+        self.__salary = salary
